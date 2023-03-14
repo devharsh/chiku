c1c8d75bd3b868d6b2bc7792b0506b87ba2ee2e1
Devharsh Trivedi <devharsh@live.in>
2023-03-14 19:19:23 -0400

    version 0.0.9 with Chebyshev series

---
+++ src/chiku/chebyshev.py
@@ -0,0 +1,26 @@
+import math
+
+
+class chebyshev:
+
+	def __init__(self, funcx=math.log, degree=5, frange=[-1,1]):
+		self.a = frange[0]
+		self.b = frange[1]
+
+		bma = 0.5 * (self.b - self.a)
+		bpa = 0.5 * (self.b + self.a)
+		f = [funcx(math.cos(math.pi * (k + 0.5) / degree) * bma + bpa) for k in range(degree)]
+		fac = 2.0 / degree
+		self.c = [fac * sum([f[k] * math.cos(math.pi * j * (k + 0.5) / degree)
+			for k in range(degree)]) for j in range(degree)]
+		print(self.c)
+
+
+	def predict(self, x):
+		y = (2.0 * x - self.a - self.b) * (1.0 / (self.b - self.a))
+		y2 = 2.0 * y
+		(d, dd) = (self.c[-1], 0)             # Special case first step for efficiency
+		for cj in self.c[-2:0:-1]:            # Clenshaw's recurrence
+			(d, dd) = (y2 * d - dd + cj, d)
+
+		return y * d - dd + 0.5 * self.c[0]   # Last step is different
